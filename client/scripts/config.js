e892ba242df9766421c5ed426c863cb30c920786
yerdzxc <rdgjr.pro@gmail.com>
2024-08-03 10:28:35 +0800

    more template cleaning

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -2,6 +2,3 @@ angular
   .module('betting.config', [])
   .constant('Server_url', 'http://localhost:4100/')
   .constant('Server_api_url', 'https://project-bet.svnts.xyz/betstar/api/')
-
-  // .constant('Server_url', 'http://199.115.119.153:4100/')
-  // .constant('Server_api_url', 'http://199.115.119.153:4100/api/')
--- server/config/development.js
+++ server/config/development.js
@@ -2,8 +2,6 @@ module.exports = {
   db: 'mongodb://unified:Asdqwe123@localhost:27017/betstar',
   server_url: "http://127.0.0.1:4100",
   server_api_url: "http://127.0.0.1:4100/api/",
-  // server_url: "http://199.115.119.153:4100",
-  // server_api_url: "http://199.115.119.153:4100/api/",
   account_eamil: "timothy0205@outlook.com",
   email_pass: "test-123000"
 }
